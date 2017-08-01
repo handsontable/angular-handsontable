806bcf72fb003fe6055514aaeb2b16dd6c937c00
swistach <swistach@gmail.com>
2017-08-01 11:27:49 +0200

    Fix: gitignore rule for config. #2

---
+++ config/webpack.test.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var helpers = require('./helpers');
+
+module.exports = {
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [helpers.root('src'), "node_modules"]
+  },
+
+  module: {
+    rules: [
+      {
+          test: /\.ts$/,
+          exclude: [
+              helpers.root('node_modules/rxjs'),
+              helpers.root('node_modules/@angular')
+          ],
+          loaders: [
+              {
+                  loader: 'awesome-typescript-loader',
+                  options: {
+                      configFileName: helpers.root('tsconfig.json')
+                  }
+              },
+              'angular2-template-loader',
+          ]
+      },
+      {
+          test: /\.css$/,
+          loaders: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    )
+  ]
+}
