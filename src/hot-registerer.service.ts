4a6546f66f90a026fc0327b3fe76943dc4005b2f
swistach <swistach@gmail.com>
2017-09-06 15:05:49 +0200

    Change: instance type in HotRegisterer. #2

--- src/hot-registerer.service.ts
+++ src/hot-registerer.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import Handsontable from 'handsontable';
 
-const instances: object = {};
+const instances = new Map<string, Handsontable>();
 
 @Injectable()
 export class HotRegisterer {
-    public getInstance(id: any): Handsontable {
+    public getInstance(id: string): Handsontable {
         return instances[id];
     }
 
-    public registerInstance(id: any, instance: Handsontable): void {
+    public registerInstance(id: string, instance: Handsontable): void {
         instances[id] = instance;
     }
 
-    public removeInstance(id: any): void {
+    public removeInstance(id: string): void {
         instances[id] = void 0;
     }
 }
