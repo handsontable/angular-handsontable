a6b4abc620a50b7760dc3afc1a02e045dbd8c68f
swistach <swistach@gmail.com>
2018-01-30 13:19:03 +0100

    Moves tests to separated files. Adds test component only for lib components. Default app component/module can be usefull to test lib on serve. #48

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
-import * as Handsontable from 'handsontable-pro';
-
-import { HotTableRegisterer } from '../../dist/pro';
 
 @Component({
   selector: 'app-root',
-  template: `<hot-table></hot-table>`
+  template: ``
 })
 export class AppComponent {
-  id: string = 'hot';
-  public prop: object = {};
 
-  constructor (
-    private _registerer: HotTableRegisterer
-  ) { }
-
-  getHotInstance(instance: string): Handsontable {
-    return this._registerer.getInstance(instance);
-  };
 }
