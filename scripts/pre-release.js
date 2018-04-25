6c4d0c4d3acf37c970e17cc2281b5246b9a9f594
swistach <swistach@gmail.com>
2018-04-25 12:43:45 +0200

    Changed releasing process - wip. #56

---
+++ scripts/pre-release.js
@@ -0,0 +1,9 @@
+(function() {
+  const fs = require('fs');
+  const path = require('path');
+
+  const SOURCE_PACKAGE = require(path.resolve('./package.json'));
+
+  console.log(SOURCE_PACKAGE)
+
+})();
