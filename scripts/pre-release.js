d995773060f9de4d27143cdb537ed629d88a4f14
swistach <swistach@gmail.com>
2018-04-25 12:57:13 +0200

    Changed release process + version sync. #56

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -1,9 +1,18 @@
 (function() {
-  const fs = require('fs');
+  const fs = require('fs-extra')
   const path = require('path');
 
-  const SOURCE_PACKAGE = require(path.resolve('./package.json'));
+  const CE_PACKAGE_PATH = path.resolve('./dist/ce/package.json');
+  const PRO_PACKAGE_PATH = path.resolve('./dist/pro/package.json');
 
-  console.log(SOURCE_PACKAGE)
+  const SOURCE_PACKAGE = require(path.resolve('./package.json'));
+  const CE_PACKAGE = require(CE_PACKAGE_PATH);
+  const PRO_PACKAGE = require(PRO_PACKAGE_PATH);
+
+  CE_PACKAGE.version = SOURCE_PACKAGE.version;
+  PRO_PACKAGE.version = SOURCE_PACKAGE.version;
+
+  fs.writeJsonSync(CE_PACKAGE_PATH, CE_PACKAGE);
+  fs.writeJsonSync(PRO_PACKAGE_PATH, PRO_PACKAGE);
 
 })();
