1b69a542e7c196d109b17021c58f1e728238278d
Piotr Laszczkowski <swistach@users.noreply.github.com>
2018-06-21 15:51:34 +0200

    Changed generation process of utils (#103)

--- scripts/copy-utils.js
+++ scripts/copy-utils.js
@@ -8,10 +8,13 @@ const README = 'README.md';
 const CE_PATH = './dist/ce';
 const PRO_PATH = './dist/pro';
 
+const SRC_CE_PATH = './lib/ce';
+const SRC_PRO_PATH = './lib/pro';
+
 fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${CE_PATH}/${LICENSE}`), { overwrite: true });
 fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${CE_PATH}/${CONTRIBUTING}`), { overwrite: true });
-fse.copySync(path.resolve(`./${README}`), path.resolve(`${CE_PATH}/${README}`), { overwrite: true });
+fse.copySync(path.resolve(`${SRC_CE_PATH}/${README}`), path.resolve(`${CE_PATH}/${README}`), { overwrite: true });
 
 fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${PRO_PATH}/${LICENSE}`), { overwrite: true });
 fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${PRO_PATH}/${CONTRIBUTING}`), { overwrite: true });
-fse.copySync(path.resolve(`./${README}`), path.resolve(`${PRO_PATH}/${README}`), { overwrite: true });
+fse.copySync(path.resolve(`${SRC_PRO_PATH}/${README}`), path.resolve(`${PRO_PATH}/${README}`), { overwrite: true });
