648fbbc5e06ba9325095ca0faf89478173e1a19a
Piotr Laszczkowski <swistach@gmail.com>
2018-12-14 17:20:01 +0100

    Changed: pre-release commands. WIP

--- scripts/copy-utils.js
+++ scripts/copy-utils.js
@@ -3,18 +3,12 @@ const path = require('path');
 
 const LICENSE = 'LICENSE';
 const CONTRIBUTING = 'CONTRIBUTING.md';
-const README = 'README.md';
 
 const CE_PATH = './dist/ce';
 const PRO_PATH = './dist/pro';
 
-const SRC_CE_PATH = './lib/ce';
-const SRC_PRO_PATH = './lib/pro';
-
 fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${CE_PATH}/${LICENSE}`), { overwrite: true });
 fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${CE_PATH}/${CONTRIBUTING}`), { overwrite: true });
-fse.copySync(path.resolve(`${SRC_CE_PATH}/${README}`), path.resolve(`${CE_PATH}/${README}`), { overwrite: true });
 
 fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${PRO_PATH}/${LICENSE}`), { overwrite: true });
 fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${PRO_PATH}/${CONTRIBUTING}`), { overwrite: true });
-fse.copySync(path.resolve(`${SRC_PRO_PATH}/${README}`), path.resolve(`${PRO_PATH}/${README}`), { overwrite: true });
