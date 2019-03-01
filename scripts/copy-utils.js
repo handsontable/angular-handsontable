5385f2473361faf9a6ca09c38f71222787577a77
Piotr Laszczkowski <swistach@gmail.com>
2019-03-01 14:38:12 +0100

    Renamed project/pro into project/hot-table. #142

--- scripts/copy-utils.js
+++ scripts/copy-utils.js
@@ -4,8 +4,8 @@ const path = require('path');
 const LICENSE = 'LICENSE';
 const CONTRIBUTING = 'CONTRIBUTING.md';
 const README = 'README.md';
-const PRO_PATH = './dist/pro';
+const TARGET_PATH = './dist/hot-table';
 
-fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${PRO_PATH}/${LICENSE}`), { overwrite: true });
-fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${PRO_PATH}/${CONTRIBUTING}`), { overwrite: true });
-fse.copySync(path.resolve(`./${README}`), path.resolve(`${PRO_PATH}/${README}`), { overwrite: true });
+fse.copySync(path.resolve(`./${LICENSE}`), path.resolve(`${TARGET_PATH}/${LICENSE}`), { overwrite: true });
+fse.copySync(path.resolve(`./${CONTRIBUTING}`), path.resolve(`${TARGET_PATH}/${CONTRIBUTING}`), { overwrite: true });
+fse.copySync(path.resolve(`./${README}`), path.resolve(`${TARGET_PATH}/${README}`), { overwrite: true });
--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -1,34 +1,34 @@
 const fse = require('fs-extra')
 const path = require('path');
 
-const PRO_PACKAGE_PATH = path.resolve('./dist/pro/package.json');
-const PRO_UMD_MIN_JS_PATH = path.resolve('./dist/pro/bundles/handsontable-angular.umd.min.js');
-const PRO_UMD_JS_PATH = path.resolve('./dist/pro/bundles/handsontable-angular.umd.js');
+const TARGET_PACKAGE_PATH = path.resolve('./dist/pro/package.json');
+const TARGET_UMD_MIN_JS_PATH = path.resolve('./dist/pro/bundles/handsontable-angular.umd.min.js');
+const TARGET_UMD_JS_PATH = path.resolve('./dist/pro/bundles/handsontable-angular.umd.js');
 
 const SOURCE_PACKAGE = require(path.resolve('./package.json'));
-const PRO_PACKAGE = fse.readJsonSync(PRO_PACKAGE_PATH, { encoding: 'utf-8' });
+const TARGET_PACKAGE = fse.readJsonSync(TARGET_PACKAGE_PATH, { encoding: 'utf-8' });
 
-PRO_PACKAGE.version = SOURCE_PACKAGE.version;
-PRO_PACKAGE.description = SOURCE_PACKAGE.description;
-PRO_PACKAGE.repository = SOURCE_PACKAGE.repository;
-PRO_PACKAGE.author = SOURCE_PACKAGE.author;
-PRO_PACKAGE.keywords = SOURCE_PACKAGE.keywords;
-PRO_PACKAGE.license = SOURCE_PACKAGE.license;
-PRO_PACKAGE.bugs = SOURCE_PACKAGE.bugs;
-PRO_PACKAGE.peerDependencies = SOURCE_PACKAGE.peerDependencies;
+TARGET_PACKAGE.version = SOURCE_PACKAGE.version;
+TARGET_PACKAGE.description = SOURCE_PACKAGE.description;
+TARGET_PACKAGE.repository = SOURCE_PACKAGE.repository;
+TARGET_PACKAGE.author = SOURCE_PACKAGE.author;
+TARGET_PACKAGE.keywords = SOURCE_PACKAGE.keywords;
+TARGET_PACKAGE.license = SOURCE_PACKAGE.license;
+TARGET_PACKAGE.bugs = SOURCE_PACKAGE.bugs;
+TARGET_PACKAGE.peerDependencies = SOURCE_PACKAGE.peerDependencies;
 
 // workaround for https://github.com/ng-packagr/ng-packagr/issues/1159
-PRO_PACKAGE.optionalDependencies = PRO_PACKAGE.dependencies;
-PRO_PACKAGE.dependencies = {};
+TARGET_PACKAGE.optionalDependencies = TARGET_PACKAGE.dependencies;
+TARGET_PACKAGE.dependencies = {};
 
-fse.writeJsonSync(PRO_PACKAGE_PATH, PRO_PACKAGE);
+fse.writeJsonSync(TARGET_PACKAGE_PATH, TARGET_PACKAGE);
 
 let licenseBody = fse.readFileSync(path.resolve('./LICENSE'), { encoding: 'utf-8' });
-licenseBody += `\nVersion: ${PRO_PACKAGE.version} (built at ${new Date().toString()})`;
+licenseBody += `\nVersion: ${TARGET_PACKAGE.version} (built at ${new Date().toString()})`;
 const licenseBanner = `/*!\n${licenseBody.replace(/^/gm, ' * ')}\n */\n`;
-const minUMDWithLicense = `${licenseBanner}${fse.readFileSync(PRO_UMD_MIN_JS_PATH, { encoding: 'utf-8' })}`;
+const minUMDWithLicense = `${licenseBanner}${fse.readFileSync(TARGET_UMD_MIN_JS_PATH, { encoding: 'utf-8' })}`;
 
-fse.writeFileSync(PRO_UMD_MIN_JS_PATH, minUMDWithLicense, { encoding: 'utf-8' });
+fse.writeFileSync(TARGET_UMD_MIN_JS_PATH, minUMDWithLicense, { encoding: 'utf-8' });
 
-const UMDWithLicense = `${licenseBanner}${fse.readFileSync(PRO_UMD_JS_PATH, { encoding: 'utf-8' })}`;
-fse.writeFileSync(PRO_UMD_JS_PATH, UMDWithLicense, { encoding: 'utf-8' });
+const UMDWithLicense = `${licenseBanner}${fse.readFileSync(TARGET_UMD_JS_PATH, { encoding: 'utf-8' })}`;
+fse.writeFileSync(TARGET_UMD_JS_PATH, UMDWithLicense, { encoding: 'utf-8' });
